63c497f4b1ce96356bf185b0ff82ed0d7ac20bc5
Airis-site <65933695+Airis-site@users.noreply.github.com>
2020-07-04 20:07:24 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+$(function () {
+    $('.slider').bxSlider({
+        auto: true,
+        speed: 1000,
+        pager: true
+    });
+
+    $('.slider3').bxSlider({
+        auto: true,
+        speed: 1000,
+        mode: 'fade',
+        captions: false
+    });
+});
